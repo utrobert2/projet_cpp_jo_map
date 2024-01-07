1413b0f3a967bb8a850111085cdc0382efce2c2c
utrobert2 <utrobert2@gmail.com>
2024-01-07 05:29:08 +0100

    commit bon 1

--- main.cpp
+++ main.cpp
@@ -2,16 +2,11 @@
 #include <iostream>
 #include <vector>
 
-struct Country {
-    std::string name;
-    // Ajoutez ici d'autres informations spécifiques au pays
-};
-
 class CountryList {
 public:
     CountryList(sf::RenderWindow& window) : window(window) {
         // Remplissez ici la liste des pays (ce peut être une liste préétablie ou dynamique)
-        countries = {{"Allemagne"}, {"Armenie"}, {"Autriche"}, {"Azerbaijan"}, {"Belgique"}, {"Bosnie-Herzégovine"}, {"Bulgarie"}, {"Croatie"}, {"Danemark"}, {"Espagne"}, {"Estonie"}, {"Finlande"}, {"France"}, {"Géorgie"}, {"Grande-Bretagne"}, {"Grèce"}, {"Hongrie"}, {"Irlande"}, {"Italie"}, {"Lituanie"}, {"Moldavie"}, {"Norvège"}, {"Pays-Bas"}, {"Pologne"}, {"Portugal"}, {"Roumanie"}, {"Serbie"}, {"Slovaquie"}, {"Slovénie"}, {"Suède"}, {"Suisse"}, {"Tchéquie"}, {"Turquie"}, {"Ukraine"}};
+        countries = {"Allemagne", "Armenie", "Autriche", "Azerbaijan", "Belgique", "Bosnie-Herzégovine", "Bulgarie", "Croatie", "Danemark", "Espagne", "Estonie", "Finlande", "France", "Géorgie", "Grande-Bretagne", "Grèce", "Hongrie", "Irlande", "Italie", "Lituanie", "Moldavie", "Norvège", "Pays-Bas", "Pologne", "Portugal", "Roumanie", "Serbie", "Slovaquie", "Slovénie", "Suède", "Suisse", "Tchéquie", "Turquie", "Ukraine"};
         font.loadFromFile("fonts/leaguespartan-bold.ttf"); // Assurez-vous de charger une police de caractères valide
     }
 
@@ -24,25 +19,15 @@ public:
         showList = false;
     }
 
-    void drawInteractiveList() {
+    void drawList() {
         if (showList) {
             sf::Text text;
             text.setFont(font);
             text.setCharacterSize(20);
             text.setFillColor(sf::Color::Black);
             for (size_t i = 0; i < countries.size(); ++i) {
-                text.setString(countries[i].name);
+                text.setString(countries[i]);
                 text.setPosition(listPosition.x, listPosition.y + static_cast<float>(i) * 30.0f);
-
-                // Afficher le pays de manière interactive
-                if (isMouseOverCountry(text)) {
-                    text.setFillColor(sf::Color::Blue); // Changez la couleur pour indiquer l'interaction
-                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-                        // Vous pouvez ajouter ici le code pour afficher des informations sur le pays
-                        std::cout << "Informations sur le pays : " << countries[i].name << std::endl;
-                    }
-                }
-
                 window.draw(text);
             }
         }
@@ -50,24 +35,73 @@ public:
 
 private:
     sf::RenderWindow& window;
-    std::vector<Country> countries;
+    std::vector<std::string> countries;
     sf::Font font;
     sf::Vector2f listPosition;
     bool showList = false;
-
-    bool isMouseOverCountry(const sf::Text& text) {
-        sf::FloatRect bounds = text.getGlobalBounds();
-        sf::Vector2f mousePosition = sf::Vector2f(sf::Mouse::getPosition(window));
-        return bounds.contains(mousePosition);
-    }
 };
 
 class InteractiveMap {
 public:
-    // ... (le reste de votre code reste inchangé)
+    InteractiveMap() : window(sf::VideoMode(2560, 1109), "Carte Interactive SFML"), countryList(window) {
+        if (!mapTexture.loadFromFile("textures/map.png")) {
+            std::cerr << "Erreur de chargement de l'image de la carte." << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+
+        if (!logoTexture.loadFromFile("textures/logoBleu.png")) {
+            std::cerr << "Erreur de chargement de l'image du logo des JO." << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+
+        mapSprite.setTexture(mapTexture);
+        logoSprite.setTexture(logoTexture);
+    }
+
+    void run() {
+        while (window.isOpen()) {
+            handleEvents();
+            update();
+            render();
+        }
+    }
 
 private:
-    // ... (le reste de votre code reste inchangé)
+    sf::RenderWindow window;
+    sf::Texture mapTexture;
+    sf::Texture logoTexture;
+    sf::Sprite mapSprite;
+    sf::Sprite logoSprite;
+    float mapScale = 1.0f;
+    sf::FloatRect rectangleBounds{1028, 174, 1446 - 1028, 377 - 174};
+    CountryList countryList;
+
+    void handleEvents() {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                    if (rectangleBounds.contains(mousePosition)) {
+                        // Afficher la liste des pays
+                        countryList.setListPosition(mousePosition);
+                    } else {
+                        // Cacher la liste si le clic est en dehors de la zone du rectangle
+                        countryList.hideList();
+                    }
+                }
+            }
+        }
+    }
+
+    void update() {
+        // Mise à jour des éléments si nécessaire
+    }
 
     void render() {
         // Effacement de la fenêtre
@@ -83,8 +117,8 @@ private:
             window.draw(logoSprite);
         }
 
-        // Affichage de la liste des pays de manière interactive
-        countryList.drawInteractiveList();
+        // Affichage de la liste des pays
+        countryList.drawList();
 
         // Affichage des changements
         window.display();
